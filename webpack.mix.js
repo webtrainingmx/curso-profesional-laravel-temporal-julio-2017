0a4591fbedb9799516e4ab1cf120bc7d23f4f790
Alex Arriaga <alex.arriaga.m@gmail.com>
2017-08-03 15:31:09 -0500

    Agregar ejemplo de integración de un JavaScript externo

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,4 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
-
+mix.copyDirectory('resources/assets/js/vendor', 'public/js/vendor');
